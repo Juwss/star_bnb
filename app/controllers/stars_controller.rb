0b8d230ea286177741e7bb878d041ce303c62fb6
Sacha Karasu <marie.karasu@gmail.com>
2022-11-21 18:00:09 +0100

    star model controller viewsss

---
+++ app/controllers/stars_controller.rb
@@ -0,0 +1,30 @@
+class StarsController < ApplicationController
+
+  def index
+    @stars = Star.all
+  end
+
+  def show
+    @star = Star.find(params[:id])
+  end
+
+  def new
+    @star = Star.new
+  end
+
+  def create
+    @star = Star.new(star_params)
+    if @star.save
+      redirect_to star_path(@star)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def star_params
+    params.require(:star).permit(:last_name, :first_name, :description, :type, :price)
+  end
+
+end
