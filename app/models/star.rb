ce94549249957faf3b44d1c6ae9511b70067f3dd
Julia Weiss <juliaweiss@live.fr>
2022-11-22 11:27:15 +0100

    creating a list of catgories

--- app/models/star.rb
+++ app/models/star.rb
@@ -1,3 +1,5 @@
 class Star < ApplicationRecord
   belongs_to :user
+  CATEGORY = ["Humourist", "Music artist", "Stylist", "Actor/actress", "Professional athlete", "Reality TV star", "Political figure"]
+  validates :category, inclusion: { in: CATEGORY }
 end
