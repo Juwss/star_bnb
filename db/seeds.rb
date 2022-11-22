ca719f66ce9d82b1a0ee729165f508065e6ecc7e
Julia Weiss <juliaweiss@live.fr>
2022-11-22 10:54:12 +0100

    adding seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Star.destroy_all
+User.destroy_all
+
+user2 = User.new(email: "tutu@gmail.com", password: "totototo2000")
+user2.save
+star1 = Star.new(first_name: "Toto", last_name: "Knowles")
+star1.user = user2
+star1.save
