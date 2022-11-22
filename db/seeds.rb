378193f8e752aea2a7aad10c010263797f7b43d7
Marianne Gilles <mariannegilles13@outlook.fr>
2022-11-22 16:42:54 +0100

    newhtml

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@ user1 = User.new(email: "james_smith@gmail.com", password: "James2000", last_nam
 user1.save
 
 
-star1 = Star.new(first_name: "Beyoncé", last_name: "Knowles", price: "1000", description: "Performeuse reconnue, Beyoncé est adepte des concerts en petit comité et saura mettre une ambiance de feu dans vos évènements")
+star1 = Star.new(first_name: "Beyoncé", last_name: "Knowles", category: "Humourist",price: "1000", description: "Performeuse reconnue, Beyoncé est adepte des concerts en petit comité et saura mettre une ambiance de feu dans vos évènements")
 star1.user = user1
 star1.save
